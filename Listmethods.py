6510dcadd9cbc799b6fa76dadc3d3e6529cf9fb6
shouryacool <84572733+shouryacool@users.noreply.github.com>
2021-10-22 23:16:04 +0530

    Add files via upload

---
+++ Listmethods.py
@@ -0,0 +1,8 @@
+l1=[1,5,2,6,4,8,7,9,3]
+l1_sorted=l1.sort() #Sorts The list
+l1.reverse() #Reverse The List
+l1.append(10)   # Adds At the end of the List
+l1.insert(3,5)   #Insert Any No. At a Given Path(Origin)
+l1.pop(2)           # Removes Any no. From the Index
+l1.remove(5)  #Removes Any No. From The Index 
+print(l1) 
